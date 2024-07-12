a40925c4bf513c78017d1a3cd8ad840300038faf
Kritika199 <68268954+Kritika199@users.noreply.github.com>
2024-07-12 15:01:01 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+function showSecondForm() {
+    document.getElementById('firstForm').style.display = 'none';
+    document.getElementById('secondForm').style.display = 'block';
+}
+
+function showFirstForm() {
+    document.getElementById('firstForm').style.display = 'block';
+    document.getElementById('secondForm').style.display = 'none';
+}
+
+function showThirdForm() {
+    document.getElementById('secondForm').style.display = 'none';
+    document.getElementById('thirdForm').style.display = 'block';
+}
